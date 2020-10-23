d805b23e9ef0237a0aaaa2db4a63e34e196943ca
Ananya Mahapatra <amahapat@i17.red.uits.iu.edu>
2020-10-22 22:54:04 -0400

    continued RNA seq workflow

--- rnaseq.sh
+++ rnaseq.sh
@@ -17,3 +17,40 @@ mkdir -p sequences
 for SRA in ${DMEL[@]}; do
   fasterq-dump -O sequences $SRA
 done
+
+FASTQS=($(find ./sequences -name "*\.fastq"))
+
+for FASTQ in ${FASTQS[@]}; do
+  NEWFILE=sequences/$(basename $FASTQ .fastq)_sampled.fastq
+  seqtk sample $FASTQ 100000 > $NEWFILE
+done
+
+###########################
+## fastq Quality Control ##
+###########################
+
+# Make output directory for quality control files.
+
+mkdir -p results/fastqc_reports
+
+# Run fastqc.
+
+FASTQS=$(find ./sequences -name "*sampled\.fastq")
+
+fastqc -o results/fastqc_reports $FASTQS
+
+################################
+## Generate STAR Genome Index ##
+################################
+
+# Make a directory to store the genome files.
+
+mkdir -p genome
+
+# Download and unpack the genome assembly.
+
+curl $ASSEMBLY | gunzip > ./genome/assembly.fasta
+
+# Download and unpack the genome annotation.
+
+curl $ANNOTATION | gunzip > ./genome/annotation.gtf
